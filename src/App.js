7c9d7205ccba00a881a3ba565be3d5823ac008f0
undefined name, please set in repo config <somewhere@example.com>
2021-12-12 11:57:42 +0930

    so I am probably doing things the hard way, but I am trying to retain my React learning whilst also trying out a bit of D3 (I want to start making some graphs).
    It's been good to play with the useEffect hook and also see the difference between using functions and classes in regards to hooks.
    
    I am still unsure why we now have hooks to provide functions with features that are already in the React.Component class, but it seems to be something people use.
    
    Anyways, I might try and make my graph project with just npm and d3, then come back to this react stuff.
    
    Still have some FCC projects to finish in React, so I don't think I am just going to forget it all. :P

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState, useRef } from 'react';
+import * as d3 from 'd3';
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +26,82 @@ function App() {
     </div>
   );
 }
+*/
+
+// Trying out:
+//   https://www.freecodecamp.org/news/how-to-get-started-with-d3-and-react-c7da74a5bd9f/
+
+
+const initialBooks = [
+  {
+      name: "Harry Potter and the Philosophers Stone",
+      author: "J. K. Rowling",
+      genre: "fantasy"
+  },{
+      name: "The Pedagogy of Freedom",
+      author: "Bell hooks",
+      genre: "non-fiction"
+  },{
+      name: "Harry Potter and the Chamber of Secrets",
+      author: "J. K. Rowling",
+      genre: "fantasy"
+  },{
+      name: "Gilgamesh",
+      author: "Derrek Hines",
+      genre: "poetry"
+  }
+];
+
+function App() {
+
+  const [books, setBooks] = useState(initialBooks);
+
+  //const refContainer = useRef('myDiv')
+
+  const d3Container = useRef(null);
+
+
+  /*
+  setBooks(books.concat(
+    {
+        name: "50 vegan dishes",
+        author: "Antti Leppänen",
+        genre: "non-fiction"
+    }
+  ));
+
+  */
+
+  useEffect( () => {
+
+    // this seems to need to be here, otherwise it won't render
+    // on reload for some reason?
+    // note: it still doesn't seem to render a second time... not sure...
+    // seems like we're setting two frameworks that manipulate the DOM to fight
+    // each other (React and D3)
+    if(d3Container.current) {
+
+      // Ok, if you declare the variable here, rather than up at the start
+      // of the function, it seems to render on every refresh.
+      // Thanks to website:
+      //  https://medium.com/@jeffbutsch/using-d3-in-react-with-hooks-4a6c61f1d102
+      const d3Books = d3.select(d3Container.current)
+
+      d3Books.style('background-color', 'blue');
+      //d3Books.style('color', 'white');
+      d3Books.append("li").text("banana")
+      d3Books.append("li").text("apple")
+
+
+    }
+  },
+    // useEffect dependency parameters (updates when the vars in this array change).
+    [d3Container.current]
+  );
+  return(
+    <d3Books ref={d3Container} />
+  );
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { component, useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//import d3 from 'd3';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +13,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
